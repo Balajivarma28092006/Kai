3073c482693707e3e057e5b5da9a50fe93cb0680
Balaji Varma <prathibharani1234567890@gmail.com>
2026-01-16 21:37:43 +0530

    added addemo video

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"sort"
 	"time"
 
 	"kai/spinner"
@@ -11,56 +9,31 @@ import (
 )
 
 func main() {
+	bar := spinner.NewProgressBar(100).
+		WithStyle(spinner.BarArrowAscii).
+		WithPrefix("Doing Something").
+		WithColor(color.New(color.FgCyan))
 
-	fmt.Println("Progress Bar Style Preview")
-	fmt.Println("==========================")
+	bar.Start()
 
-	for name, style := range spinner.BarStyles {
-		fmt.Printf("\nStyle: %s\n", name)
-
-		bar := spinner.NewProgressBar(100).
-			WithStyle(style).
-			WithPrefix(fmt.Sprintf("%-15s ", name)).
-			WithColor(color.New(color.FgCyan))
-
-		bar.Start()
-
-		for i := 0; i <= 100; i++ {
-			bar.Update(i)
-			time.Sleep(15 * time.Millisecond)
-		}
-
-		bar.Finish()
-		time.Sleep(300 * time.Millisecond)
+	for i := 0; i <= 100; i++ {
+		bar.Update(i)
+		time.Sleep(15 * time.Millisecond)
 	}
 
-	fmt.Println("\nAll bar styles previewed.")
+	bar.Finish()
+	time.Sleep(300 * time.Millisecond)
 
-	fmt.Println("Progress Spinner Style Preview")
-	fmt.Println("==========================")
+	s := spinner.NewSpinner().
+		WithCharsetID(52).
+		WithColor(color.New(color.FgGreen)).
+		WithSpeed(100 * time.Millisecond)
 
-	ids := make([]int, 0, len(spinner.CharSets))
-	for id := range spinner.CharSets {
-		ids = append(ids, id)
+	s.Start()
+	for i := 0; i < 10; i++ {
+		time.Sleep(5000 * time.Millisecond)
 	}
-	sort.Ints(ids)
+	s.Stop()
 
-	for _, id := range ids {
-		fmt.Printf("Style ID: %d\n", id)
-
-		s := spinner.NewSpinner().
-			WithCharsetID(id).
-			WithPrefix(fmt.Sprintf("[%02d] ", id)).
-			WithColor(color.New(color.FgGreen)).
-			WithSpeed(80 * time.Millisecond)
-
-		s.Start()
-		time.Sleep(800 * time.Millisecond)
-		s.Stop()
-
-		time.Sleep(120 * time.Millisecond)
-	}
-
-	fmt.Println("\n All spinner styles previewed.")
-
+	time.Sleep(120 * time.Millisecond)
 }
